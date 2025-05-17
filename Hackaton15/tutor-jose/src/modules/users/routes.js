5d4e79cfe7072ad45ea5d490ea39094cb39704a1
José Abraham Montenegro Zelada <jose.montenegro@bam.com.gt>
2025-05-17 16:30:15 -0500

    update hackathon 15

---
+++ Hackaton15/tutor-jose/src/modules/users/routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createUser } from "./controllers.js";
+import validateJwt from "../../middleware/validate-jwt.js";
+
+const userRoutes = Router();
+
+userRoutes.post("/", createUser);
+// userRoutes.patch("/:userId", validateJwt, updateUser);
+
+export default userRoutes;
